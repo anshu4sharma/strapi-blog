438982585797c24f35f5b9f1a49fbcd5ab879245
anshu0x <anshusharma6327@gmail.com>
2023-02-19 15:21:08 +0530

    custom api for posts removeed instead of that api/posts?filters[slug][]=how-to-stay-safe-from-airdrop&populate=* --- can be used for single post

--- src/api/post/controllers/post.js
+++ src/api/post/controllers/post.js
@@ -6,13 +6,4 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::post.post', ({ strapi }) => ({
-  async findOne(ctx) {
-    const { slug } = ctx.params
-    const entity = await strapi.db.query('api::post.post').findOne({
-      where: { slug },
-    });
-    const sanitizedEntity = await this.sanitizeOutput(entity)
-    return this.transformResponse(sanitizedEntity)
-  }
-}));
+module.exports = createCoreController('api::post.post');
